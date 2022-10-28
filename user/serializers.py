5f24ccf41776870dca422db5ecbfa3c83092c4cc
Madat Bay <medet.bayramov22@gmail.com>
2022-10-28 23:01:50 +0400

    add(user): user api views add

---
+++ user/serializers.py
@@ -0,0 +1,53 @@
+from django.contrib.auth.password_validation import validate_password
+from django.utils import timezone
+
+from rest_framework import serializers
+
+from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
+
+
+class UserSerializer(serializers.ModelSerializer):
+    date_joined = serializers.ReadOnlyField()
+    last_login = serializers.ReadOnlyField()
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "username", "email", "date_joined", "last_login")
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        required=True, validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    email = serializers.EmailField(
+        required=True, validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password", "password2", "first_name", "last_name")
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."}
+            )
+
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            full_name=validated_data["full_name"],
+            username=validated_data["username"],
+            email=validated_data["email"],
+        )
+
+        user.set_password(validated_data["password"])
+        user.save()
+
+        return user
--- user/views.py
+++ user/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+from django.contrib.auth.models import User
+
+from .serializers import UserSerializer, RegisterSerializer
+
+
+class UserViewSet(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet):
+    queryset = User.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return RegisterSerializer
+        return UserSerializer
+
+    def get_permissions(self):
+        if self.action in ["create"]:
+            self.permission_classes = [AllowAny]
+        else:
+            self.permission_classes = [IsAuthenticated]
+        return super(UserViewSet, self).get_permissions()
+
+    def get_object(self):
+        return self.request.user
