81bfd6dd27b3805d39997d90f9fe3aebb31ff667
Madat Bay <medet.bayramov22@gmail.com>
2022-11-11 17:47:18 +0400

    fix: allowed_host add

--- dews/settings.py
+++ dews/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-8psy)dc$p8(8cs+cx+fklomrk@x--i-677nbut_szr2_@+@_xv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
